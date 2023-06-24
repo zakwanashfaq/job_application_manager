e81302bea325a332b29dd1370e45bd2703d703ca
Zakwan Ashfaq Zian <zakwanashfaq@gmail.com>
2023-06-23 23:18:07 -0230

    initial implementation working

--- src/App.js
+++ src/App.js
@@ -4,19 +4,20 @@ import ApplicationList from "./components/applicationsList";
 import Item from "./components/item";
 import { useSelector, useDispatch } from 'react-redux'
 import AddItem from "./components/addItem";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { fetchData } from "./redux/items";
-
-
-
+import { ReactSortable } from "react-sortablejs";
 
 
 function App() {
   const data = useSelector((state) => [...state.items.value]);
+  console.log(data);
+  const [datalist, setDataList] = useState([]);
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(fetchData());
+    setDataList(data);
   }, [dispatch]) 
 
   
@@ -25,6 +26,14 @@ function App() {
       <div className="container-xl">
         <Header />
         <AddItem />
+        <div className="zzz">
+          <ReactSortable list={datalist} setList={setDataList}>
+            {data?.map((item) => {
+              console.log(item);
+              return <Item key={item.id} applied={item.applied} name={item.name} link={item.link} timeAdded={item.timeAdded}/>
+            })}
+          </ReactSortable>
+        </div>
         <ApplicationList>
           {data.map(item => {
             return <Item key={item.id} applied={item.applied} name={item.name} link={item.link} timeAdded={item.timeAdded}/>
