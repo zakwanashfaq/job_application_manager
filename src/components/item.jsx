15bf25c518d57baa15651f253e99a201d7ea2e0b
Zakwan Ashfaq Zian <zakwanashfaq@gmail.com>
2023-08-14 15:08:50 -0230

    moved delete button

--- src/components/item.jsx
+++ src/components/item.jsx
@@ -4,7 +4,7 @@ import { useDispatch } from 'react-redux';
 import { deleteItem } from '../redux/items';
 
 function EditItem(props) {
-    return <div className='text-body-secondary'>
+    return <div className=''>
         {/* eslint-disable-next-line */}
         <a data-bs-toggle="modal" data-bs-target="#editModal">
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-pencil-square" viewBox="0 0 16 16">
@@ -14,7 +14,7 @@ function EditItem(props) {
         </a>
 
         <div className="modal fade" id="editModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div className="modal-dialog">
+            <div className="modal-dialog" id="editModalBox">
                 <div className="modal-content">
                     <div className="modal-header">
                         <h1 className="modal-title fs-5" id="exampleModalLabel">Edit item</h1>
@@ -92,7 +92,16 @@ function Item(props){
                     {shortLink}
                 </a>
             </span>
-            <span className="col-12 col-sm-12 col-md-2 ps-2">{(new Date(props.timeAdded)).toLocaleString()}</span>
+            <span className="item-end-column col-12 col-sm-12 col-md-2">
+                <div className="div date-box">
+                    {(new Date(props.timeAdded)).toLocaleString()}
+                </div>
+                <div className='item-delete-button ps-2' onClick={handleOnDelete}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-trash3-fill" viewBox="0 0 16 16">
+                        <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5Zm-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5ZM4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06Zm6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528ZM8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5Z" />
+                    </svg>
+                </div>
+            </span>
         </div>
     );
 }
