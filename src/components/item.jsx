f7bdee9a4ff55a9beef540b33a825d256fafa4da
Zakwan Ashfaq Zian <zakwanashfaq@gmail.com>
2023-08-14 21:00:10 -0230

    fixed popup modal not showing unique ids for items

--- src/components/item.jsx
+++ src/components/item.jsx
@@ -4,17 +4,17 @@ import { useDispatch } from 'react-redux';
 import { deleteItem } from '../redux/items';
 
 function EditItem(props) {
-    // const target_id = "editModal-" + props.id; 
+    const target_id = "editModal-" + props.id; 
     return <div className=''>
         {/* eslint-disable-next-line */}
-        <a data-bs-toggle="modal" data-bs-target="#editModal">
+        <a data-bs-toggle="modal" data-bs-target={"#"+target_id}>
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-pencil-square" viewBox="0 0 16 16">
                 <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
                 <path fillRule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z" />
             </svg>
         </a>
 
-        <div className="modal fade" id="editModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div className="modal fade" id={target_id} tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
             <div className="modal-dialog" id="editModalBox">
                 <div className="modal-content">
                     <div className="modal-header">
@@ -67,7 +67,7 @@ function Item(props){
 
     return (
         <div className="item-container p-4" id={props.id}>
-            <div className=" item-hover col-11 col-sm-11 col-md-6">
+            <div className=" item-hover col-11 col-sm-11 col-md-5">
                 <div className='item-hover-container'>
                     <div className="item-dragDrop">
                         <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-grip-vertical" viewBox="0 0 16 16">
@@ -93,9 +93,9 @@ function Item(props){
                     {shortLink}
                 </a>
             </span>
-            <span className="item-end-column col-12 col-sm-12 col-md-2">
+            <span className="item-end-column col-12 col-sm-12 col-md-3">
                 <div className="div date-box">
-                    {(new Date(props.timeAdded)).toLocaleString()}
+                    {(new Date(props.timeAdded)).toUTCString().toLocaleString()}
                 </div>
                 <div className='item-delete-button ps-2' onClick={handleOnDelete}>
                     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-trash3-fill" viewBox="0 0 16 16">
